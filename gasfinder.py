aba4d66771be017cf280f3c247b321d49f6ff75e
otubo <eduardo.otubo@gmail.com>
2010-03-21 06:15:30 +0000

    removing some debug messages, printing the list

--- gasfinder.py
+++ gasfinder.py
@@ -5,25 +5,36 @@ import yql
 import android
 import time
 
+gas_station_name = []
 droid = android.Android()
-location = droid.getLastKnownLocation().result
 
-#droid.startLocating()
-#time.sleep(15)
-#location = droid.readLocation().result
+droid.startLocating()
+time.sleep(15)
+location = droid.readLocation().result
+
+#XXX: must find a way to check if the GPS returned
+#     or not.
+#location = droid.getLastKnownLocation().result
 
 longitude = location['longitude']
 latitude = location['latitude']
 
-print latitude
-print longitude
-
 URL = "USE 'http://www.meusgastos.com.br/odt/meuspostos.precos.coordenadas.xml' as meuspostos.precos.coordenadas; SELECT * FROM meuspostos.precos.coordenadas WHERE lat=%.4f and lon=%.4f" % (latitude,longitude)
 
 y = yql.Public()
-res = y.execute(URL)
+full_result = y.execute(URL)
 
-for row in res['query']['results']['item']:
-    print row['nome']
+for row in full_result['query']['results']['item']:
+    gas_station_name.append(row['nome'])
 
-#res.rows
+droid.dialogCreateAlert("Gas Stations near you")
+droid.dialogSetItems(gas_station_name)
+droid.dialogShow()
+user_choice = droid.dialogGetResponse().result
+
+#XXX: And this is the worst way to find a record. Please fix me
+for row in full_result['query']['results']['item']:
+    if row['nome'] == user_choice:
+        gas_station_coordinate = "%.4f,%.4f" % (row['latitude'],row['longitude'])
+        droid.map(gas_station_coordinate)
+
