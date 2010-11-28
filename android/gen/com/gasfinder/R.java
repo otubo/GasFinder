43f89ddf70fc392515e061eeb99dfbd8422af819
otubo <eduardo.otubo@gmail.com>
2010-11-28 00:44:53 -0200

    Running on a single file, example working copy

--- android/gen/com/gasfinder/R.java
+++ android/gen/com/gasfinder/R.java
@@ -17,7 +17,11 @@ public final class R {
         public static final int icon=0x7f020003;
     }
     public static final class id {
-        public static final int mapview=0x7f050000;
+        public static final int empty=0x7f050002;
+        public static final int list=0x7f050001;
+        public static final int main=0x7f050000;
+        public static final int mainlayout=0x7f050003;
+        public static final int mapview=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
